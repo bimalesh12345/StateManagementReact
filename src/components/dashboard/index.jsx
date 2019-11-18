3234c992dad9fea598bc66be1d340e87080a93cd
bimalesh chauhan <bimalesh12345@gmail.com>
2019-11-18 21:50:18 +0530

    updates form

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -4,25 +4,39 @@ const Admin = () => {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [movies, setMovies] = useContext(MovieContext);
+  const [nameerror, setnameError] = useState(0);
+  const [priceerror, setpriceError] = useState(0);
   const updateName = e => {
+    setnameError(0);
     setName(e.target.value);
   };
   const updatePrice = e => {
+    setpriceError(0);
     setPrice(e.target.value);
   };
   const addMovies = e => {
     e.preventDefault();
+    if (name == "") {
+      setnameError(1);
+      return;
+    } else if (price == "") {
+      setpriceError(1);
+      return;
+    }
 
     setMovies(prevMovies => [
       ...prevMovies,
       { name: name, price: price, id: movies.length + 1 }
     ]);
+    setPrice("");
+    setName("");
   };
   return (
     <div className="addmovies">
       <form onSubmit={addMovies}>
         <input
           type="text"
+          className={nameerror == "1" ? "error" : ""}
           name="name"
           value={name}
           onChange={updateName}
@@ -30,6 +44,7 @@ const Admin = () => {
         />
         <input
           type="text"
+          className={priceerror == "1" ? "error" : ""}
           name="price"
           value={price}
           onChange={updatePrice}
